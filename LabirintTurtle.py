8b74cdb5b0b9ee024e44b250ea3a4e59cbee0605
SanGReaL53 <58322074+SanGReaL53@users.noreply.github.com>
2020-12-01 18:19:15 +0300

    Update LabirintTurtle.py

--- LabirintTurtle.py
+++ LabirintTurtle.py
@@ -4,16 +4,20 @@ class LabirintTurtle():
         self.turtle = []
         self.exit = 0
         self.exitc = []
-        self.labirint111 = []
+        self.lab = []
 
     def load_map(self, file):
         try:
+            line = 0
             f = open(file, 'r')
             for line in f:
                 if line.startswith(' ') or line.startswith('*'):
                     self.map.append(list(line[:-1]))
                 else:
-                    self.turtle.append(int(line))
+                    break
+            pos_x = int(line)
+            pos_y = int(f.readline())
+            self.turtle = [pos_x, pos_y]
             self.check_map()
         except ValueError:
             print('Error')
@@ -52,56 +56,47 @@ class LabirintTurtle():
         if self.map[self.turtle[0]][self.turtle[1]] == '*':
             return False
     def find_way(self):
-        self.labirint111 = self.map
+        self.lab = self.map
 
  
  
 
 
  
-        y=self.turtle[0]
-        x=self.turtle[1]
-        self.labirint111[y][x]=1 
-        for y in range(0, len(self.labirint111)):
-            for x in range(0, len(self.labirint111[y])):
-                 if self.labirint111[y][x]==' ':
-                     self.labirint111[y][x]= 0
+        x = self.turtle[0]
+        y = self.turtle[1]
+        self.lab[x][y] = 1 
+        for i in range(0, len(self.lab)):
+            for j in range(0, len(self.lab[i])):
+                 if self.lab[i][j] == ' ':
+                     self.lab[i][j] = 0
         
  
  
-        for yy in range(0, len(self.labirint111)-1):
-            for xx in range(0, len(self.labirint111[yy])-1):
+        for i in range(0, len(self.lab) - 1):
+            for j in range(0, len(self.lab[i]) - 1):
  
-                for y in range(0, len(self.labirint111) - 1):
-                    for x in range(0, len(self.labirint111[y]) - 1):
-                        if self.labirint111[y][x] != '*':
+                for x in range(0, len(self.lab) - 1):
+                    for y in range(0, len(self.lab[x]) - 1):
+                        if self.lab[x][y] != '*':
  
                         
-                            if self.labirint111[y - 1][x] != '*' and self.labirint111[y - 1][x] == 0 and self.labirint111[y][x] != 0:
-                                self.labirint111[y - 1][x] = int(self.labirint111[y][x]) + 1
+                            if self.lab[x - 1][y] != '*' and self.lab[x - 1][y] == 0 and self.lab[x][y] != 0:
+                                self.lab[x - 1][y] = int(self.lab[x][y]) + 1
                         
-                            if self.labirint111[y][x + 1] != '*' and self.labirint111[y][x + 1] == 0 and self.labirint111[y][x] != 0:
-                                self.labirint111[y][x + 1] = int(self.labirint111[y][x]) + 1
+                            if self.lab[x][y + 1] != '*' and self.lab[x][y + 1] == 0 and self.lab[x][y] != 0:
+                                self.lab[x][y + 1] = int(self.lab[x][y]) + 1
                         
-                            if self.labirint111[y + 1][x] != '*' and self.labirint111[y + 1][x] == 0 and self.labirint111[y][x] != 0:
-                                self.labirint111[y + 1][x] = int(self.labirint111[y][x]) + 1
+                            if self.lab[x + 1][y] != '*' and self.lab[x + 1][y] == 0 and self.lab[x][y] != 0:
+                                self.lab[x + 1][y] = int(self.lab[x][y]) + 1
                         
-                            if self.labirint111[y][x - 1] != '*' and self.labirint111[y][x - 1] == 0 and self.labirint111[y][x] != 0:
-                                self.labirint111[y][x - 1] = int(self.labirint111[y][x]) + 1
+                            if self.lab[x][y - 1] != '*' and self.lab[x][y - 1] == 0 and self.lab[x][y] != 0:
+                                self.lab[x][y - 1] = int(self.lab[x][y]) + 1
  
  
  
 
-        for y in range(0, len(self.labirint111)):
-            for x in range(0, len(self.labirint111[y])):
-                if self.labirint111[y][x]=='':
-                    self.labirint111[y][x]=1
-
-    
-            
-t = LabirintTurtle()
-t.load_map('l1.txt')
-t.show_map(True)
-t.check_map()
-print(t.exitc)
-t.find_way()
+        for i in range(0, len(self.lab)):
+            for j in range(0, len(self.lab[i])):
+                if self.lab[i][j] == '':
+                    self.lab[i][j] = 1
